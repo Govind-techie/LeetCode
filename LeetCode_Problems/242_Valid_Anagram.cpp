59f81c60d928dbfb04631ac3531cc3a347108053
govindchoudhary0981@gmail.com <govindchoudhary0981@gmail.com>
2025-06-12 17:03:12 +0530

    updated valid anagram problem with more optimisation using hashTable

--- LeetCode_Problems/242_Valid_Anagram.cpp
+++ LeetCode_Problems/242_Valid_Anagram.cpp
@@ -32,45 +32,32 @@ Constraints:
 s and t consist of lowercase English letters.
 */
 
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <unordered_map>
+#include <string>
 using namespace std;
 
 // Function to check if two strings are anagrams
 bool isAnagram(string s, string t) {
-    // If lengths are different, they can't be anagrams
-    if (s.length() != t.length()) 
-        return false;
+    if (s.size() != t.size()) return false;
 
-    int count[26] = {0}; // Array to count frequency of each character (assuming only lowercase letters)
+    unordered_map<char, int> m1;
+    unordered_map<char, int> m2;
 
-    // Count characters from first string
-    for (int i = 0; i < s.length(); i++) {
-        count[s[i] - 'a']++;
-    }
+    for (char ch : s) m1[ch]++;
+    for (char ch : t) m2[ch]++;
 
-    // Decrease count based on second string
-    for (int i = 0; i < t.length(); i++) {
-        if (count[t[i] - 'a'] == 0) {
-            return false; // If character not found or overused
-        }
-        count[t[i] - 'a']--;
-    }
-
-    return true; // All counts matched, so they are anagrams
+    return m1 == m2; // compare the two entire maps
 }
 
 int main() {
-    string str1, str2;
-    cout << "Enter first string: ";
-    cin >> str1;
-    cout << "Enter second string: ";
-    cin >> str2;
+    string s = "anagram";
+    string t = "nagaram";
 
-    if (isAnagram(str1, str2)) {
-        cout << "Valid Anagram" << endl;
+    if (isAnagram(s, t)) {
+        cout << s << " and " << t << " are anagrams." << endl;
     } else {
-        cout << "Not an Anagram" << endl;
+        cout << s << " and " << t << " are not anagrams." << endl;
     }
 
     return 0;
